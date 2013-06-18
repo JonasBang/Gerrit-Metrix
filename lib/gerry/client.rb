ae66ab5bcdc8aeee01732c2d41400aa010bee446
Jonas Bang Christensen <jonas@atchik.com>
2013-06-18 10:56:04 +0200

    Add lib "gerry".

---
+++ lib/gerry/client.rb
@@ -0,0 +1,28 @@
+require 'httparty'
+require 'json'
+
+module Gerry
+  class Client
+    include HTTParty
+    headers 'Accept' => 'application/json'
+    
+    require_relative 'client/accounts'
+    require_relative 'client/changes'
+    require_relative 'client/projects'
+    require_relative 'client/request'
+    
+    include Accounts
+    include Changes
+    include Projects
+    include Request
+    
+    def initialize(url, username = nil, password = nil)
+      self.class.base_uri(url)
+      
+      if username && password      
+        @username = username
+        @password = password
+      end
+    end
+  end
+end
