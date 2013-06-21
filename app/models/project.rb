1d44f1d24efc710a3a448a5a2d8d8c437b3b5e74
Jonas Bang Christensen <jonas@atchik.com>
2013-06-21 13:21:11 +0200

    Fix: Minor change in validates_uniqueness_of

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,5 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :server
   
   validates_presence_of :name, :server
-  validates_uniqueness_of :name, :case_sensitive => false, :scope => :server_id
+  validates_uniqueness_of :name, :case_sensitive => false, :scope => :server
 end
