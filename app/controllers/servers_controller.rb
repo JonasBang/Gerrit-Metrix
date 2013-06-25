ea143e6d7cb1f0c322e13afbafdc7e87d80fe8fe
Jonas Bang Christensen <jonas@atchik.com>
2013-06-25 09:35:35 +0200

    Add query string for projects.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -43,6 +43,7 @@ class ServersController < ApplicationController
   end
 
   def import
+    # TODO: Flash message "Import running ..."
     @server = Server.find params[:id]
     gerrit = Gerry.new(@server.url, @server.username, @server.password)
     projects_list = gerrit.projects
@@ -54,6 +55,9 @@ class ServersController < ApplicationController
           # TODO: Handle failing save
         end
       end
+#      query_string = "q=status:merged+project:#{project_name}+-age:1week&o=ALL_REVISIONS&o=ALL_COMMITS&o=DETAILED_LABELS&o=DETAILED_ACCOUNTS&o=CURRENT_FILES"
+      query_string = "q=status:merged+project:#{project_name}+-age:1month&o=ALL_REVISIONS&o=ALL_COMMITS&o=DETAILED_LABELS&o=DETAILED_ACCOUNTS&o=CURRENT_FILES"
+      project_changes = gerrit.changes([query_string])
     }
 
     redirect_to servers_path, :flash => { :notice => "Import successful." }
